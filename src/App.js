1f3efc213bd64f545f45de4b0565bc75c78bf97e
solveius <47314869+solveius@users.noreply.github.com>
2019-02-27 10:26:00 +0100

    Completed the module

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import Quiz from './quiz.js'
 import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div />
+      <Quiz />
     )
   }
 }
