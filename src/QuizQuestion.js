96cb4d93e55518e951a5f104626e320d173e60a6
Solveig Uv Sundlisæter <solveius@gmail.com>
2019-03-01 15:46:48 +0100

    Completed the module

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -35,7 +35,7 @@ class QuizQuestion extends Component {
           })}
         </ul>
       </section>
-      {this.state.incorrectAnswer ? <p className='error'>Sorry, that's not correct</p> : null}
+      {this.state.incorrectAnswer ? <p className='error'>Sorry, that's not right</p> : null}
     </main>
     )
   }
